0b1a8f45af22997443447c9d3fb2b68b5fc8c13d
holabayor <liasu.olabayo@gmail.com>
2023-07-16 23:15:45 +0100

    utils setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,22 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import cors from 'cors';
+import dbClient from './utils/db';
 
 const app = express();
+const port = process.env.PORT || 8000;
 
-app.use();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json('Contact Manager');
+});
+
+app.listen(port, () => {
+  dbClient();
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ backend/utils/db.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+
+const DATABASE = process.env.DB || 'contact_manager';
+const url = process.env.MONGODB_URL;
+
+console.log(url);
+
+const dbClient = () => {
+  mongoose.connect(url).then(() => {
+    console.log(`${DATABASE} is connected to MongoDB successfully.`);
+  });
+};
+
+dbClient();
