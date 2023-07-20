cde64d1e9c771cd5a1d83a2e0154c9ce955c6e5a
holabayor <liasu.olabayo@gmail.com>
2023-07-20 17:02:47 +0100

    user model

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,23 @@
+import User from '../models/user';
+
 class AuthController {
-  static signup(req, res) {
-    res.json('Signup');
+  static async signup(req, res) {
+    try {
+      const { firstName, lastName, email, password } = req.body;
+
+      // Check if the user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(409).json({ error: 'User already exists' });
+      }
+
+      // Create a new user
+      const newUser = User({ firstName, lastName, email, password });
+      await newUser.save();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
   }
 
   static login(req, res) {
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 // Define the user schema
 const userSchema = new mongoose.Schema(
@@ -36,6 +37,21 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Hash the password before saving the user data
+userSchema.pre('save', async function (next) {
+  try {
+    // Check if the password is modified or is new (for new user)
+    if (this.isModified('password') || this.isNew) {
+      const salt = await bcrypt.genSalt(10); // Generate a salt
+      const hashedPassword = await bcrypt.hash(this.password, salt); // Hash the password
+      this.password = hashedPassword; // Replace the plain password with the hashed one
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 // Create the User model based on the user schema
 const User = mongoose.model('User', userSchema);
