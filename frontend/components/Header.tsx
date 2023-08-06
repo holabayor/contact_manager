54c9667b6e9d2daa0943e137462a09076170789b
holabayor <liasu.olabayo@gmail.com>
2023-08-06 20:23:00 +0100

    initial commit

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1 className="text-center text-4xl font-bold text-gray-800 dark:text-white">Contact Manager</h1>
+        </header>
+    )
+}
+
+export default Header;
