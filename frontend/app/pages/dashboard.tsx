3310c9f652776f31f2c5f3cf76db5aa380e739e5
holabayor <liasu.olabayo@gmail.com>
2023-08-09 20:19:25 +0100

    initial commit

--- frontend/app/pages/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dashboard = () => {
-    return (
-        <div>Contact Manager</div>
-    )
-}
-
-export default Dashboard
--- frontend/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header: React.FC = () => {
-    return (
-        <header>
-            <h1 className="text-center text-4xl font-bold text-gray-800 dark:text-white">Contact Manager</h1>
-        </header>
-    )
-}
-
-export default Header;
