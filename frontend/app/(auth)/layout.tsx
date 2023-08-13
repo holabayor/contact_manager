f7daa08e0a5e8b1c907b7bae92c655cb1f3f6cfa
holabayor <liasu.olabayo@gmail.com>
2023-08-13 19:21:50 +0100

    layout

---
+++ frontend/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import Sidebar from '@/components/Sidebar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Contact Manager',
+  description: 'Simple contact manager',
+}
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
